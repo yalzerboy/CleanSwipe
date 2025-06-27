a1044d4dd9245aa03f0795e3a94d29409b642765
Yalun Zhang <yalun@live.co.uk>
2025-06-27 21:03:54 +0100

    Initial Commit

---
+++ CleanSwipe/CleanSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanSwipeApp.swift
+//  CleanSwipe
+//
+//  Created by Yalun Zhang on 27/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CleanSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanSwipeTests/CleanSwipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanSwipeTests.swift
+//  CleanSwipeTests
+//
+//  Created by Yalun Zhang on 27/06/2025.
+//
+
+import Testing
+@testable import CleanSwipe
+
+struct CleanSwipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
